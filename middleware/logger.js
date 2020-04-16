f357b421ccf15f5fe6fc9f54686ada3af9b5dd25
Jane Suh <sourcherryyogurt@gmail.com>
2020-04-16 18:07:08 -0400

    Completed initial project set up, added logger middleware function

---
+++ middleware/logger.js
@@ -0,0 +1,10 @@
+const logger = () => {
+    return (req, res, next) => {
+        console.log(
+            `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
+        );
+        next();
+    }
+};
+
+module.exports = logger;
